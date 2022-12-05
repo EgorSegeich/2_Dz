1c1fa321e1426fa94a56c75d148247a145b7286a
Egor <aldes@list.ru>
2022-12-06 00:06:38 +0300

    2 task done

---
+++ 2_task/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("введите любое число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 99)
+{
+    string X = Convert.ToString(N);
+    Console.WriteLine(X[2]);
+}
+else
+{
+   Console.WriteLine("третьей цифры нет"); 
+}
+
+
+
+
+/*
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (N > 99)
+{
+   if (N < 999)
+    {
+    int i = N % 10;
+    Console.WriteLine(i);
+    }
+    else
+    {
+    int i = N/10 % 10;
+    Console.WriteLine(i);
+    }
+}
+else
+{
+   Console.WriteLine("третьей цифры нет"); 
+}
+*/
