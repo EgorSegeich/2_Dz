a02e6061273d75d179e43e82213d74438fe95345
Egor <aldes@list.ru>
2022-12-06 00:08:53 +0300

    2 task done

--- 2_task/Program.cs
+++ 2_task/Program.cs
@@ -10,30 +10,3 @@ else
 {
    Console.WriteLine("третьей цифры нет"); 
 }
-
-
-
-
-/*
-int N = Convert.ToInt32(Console.ReadLine());
-
-
-
-if (N > 99)
-{
-   if (N < 999)
-    {
-    int i = N % 10;
-    Console.WriteLine(i);
-    }
-    else
-    {
-    int i = N/10 % 10;
-    Console.WriteLine(i);
-    }
-}
-else
-{
-   Console.WriteLine("третьей цифры нет"); 
-}
-*/
