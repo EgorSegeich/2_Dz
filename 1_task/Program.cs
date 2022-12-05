5dab840140c6c5ec544efb72e153cbea5461c547
Egor <aldes@list.ru>
2022-12-05 23:22:33 +0300

    1 task done

--- 1_task/Program.cs
+++ 1_task/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите трёхзначное число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int second = N /10 % 10;
+Console.WriteLine(second);
