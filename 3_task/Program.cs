040aa0b4828389491dd9a398d0af78a81962623c
Egor <aldes@list.ru>
2022-12-06 00:14:54 +0300

    3 task done

---
+++ 3_task/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("введите любое число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if ((N % 7) == 0 )
+{
+    if ((N % 23) == 0 )
+    {
+        Console.WriteLine("да");
+    }
+    else
+    { 
+    Console.WriteLine("нет");
+    }
+}
+else
+{
+    Console.WriteLine("нет");
+}
